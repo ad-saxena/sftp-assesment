533b42f07d14083543392240d95205d08441be9e
ad-saxena <adsaxena@Ads-MacBook-Air-2.local>
2023-05-07 23:13:48 +0530

    python version corrected for lambda

--- main.tf
+++ main.tf
@@ -188,7 +188,7 @@ resource "aws_lambda_function" "sftp_lambda" {
   function_name = "sftp_lambda"
   role          = aws_iam_role.lambda_role.arn
   handler       = "sftp_lambda.handler"
-  runtime       = "python3.9"
+  runtime       = "python3.8"
   timeout       = 60
 
   environment {
