4106660723894b4ba9319432fbb2c2ab26f45d95
ad-saxena <adsaxena@Ads-MacBook-Air-2.local>
2023-05-09 02:15:22 +0530

    Point backend to correct TFC org and workspace 2

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
-  cloud {
-    organization = "amandeepsaxena"
-
-    workspaces {
-      name = "terraform-github-actions"
-    }
-  }
-}
 provider "aws" {
   region = "eu-west-1"
   profile = "terraform"
